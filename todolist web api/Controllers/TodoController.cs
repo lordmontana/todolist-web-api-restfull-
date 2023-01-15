6b187e34557de932a384b5f8497ad88914523a80
lordmontana <charamis@hotmail.com>
2023-01-15 14:07:48 +0200

    Add project files.

---
+++ todolist web api/Controllers/TodoController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    private readonly TodoContext _context;
+
+    public TodoController(TodoContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/todos
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+        return await _context.TodoItems.ToListAsync();
+    }
+
+    // GET: api/todos/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        return todoItem;
+    }
+
+    // POST: api/todos
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+    {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+    }
+
+    // PUT: api/todos/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+    {
+        if (id != todoItem.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(todoItem).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/todos/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<TodoItem>> DeleteTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        _context.TodoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+
+        return todoItem;
+    }
+
+    private bool TodoItemExists(int id)
+    {
+        return _context.TodoItems.Any(e => e.Id == id);
+    }
+}
---
+++ todolist web api/ToDoItem.cs
@@ -0,0 +1,7 @@
+﻿public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
